40383adcedd94de7ffa5a477d28594b69b1b62b4
= <=>
2011-10-15 13:03:49 +0100

    tests on user class, authentication and registeration

---
+++ spec/user_spec.rb
@@ -0,0 +1,34 @@
+require '../models/user'
+
+describe User do
+  before :each do
+    @user = User.new
+  end
+
+  it "should not authenticate me with invalid credentials" do
+    status = @user.try_login('darik','unknown')
+    status.should == false
+  end
+
+  it "should authenticate me with valid credentials" do
+    status = @user.try_login('darik', 'darik123')
+    status.should == true
+  end
+
+  it "should authenticate me with valid credentials" do
+    status = @user.try_login('almas', 'brother')
+    status.should == true
+  end
+
+  it "should return false if username have already taken by someone" do
+    input = {'username' => 'darik', 'password' => 'passwd'}
+    status = @user.register(input)
+    status.should == false
+  end
+
+  it "should return true if username is available and insert it into database" do
+    input = {'username' => 'almas', 'password' => 'brother'}
+    status = @user.register(input)
+    status.should == false
+  end
+end
