2663c93672d3b7b2997382ffe4ae266ea0b0cfb8
= <=>
2011-11-01 10:11:45 +0000

    refactoring comments

--- models/comment.rb
+++ models/comment.rb
@@ -6,16 +6,8 @@ class Comment < Model
   
   def add_comment(map)
     return false if !validate_add(map)
-    #desc = map[:description]
-    #return false if desc == nil
-    #user_id = map[:user_id]
-    #return false if user_id == nil
-    #pic_id = map[:pic_id]
-    #return false if pic_id == nil
     
-    #db = MysqlConnect.new
     sql = "INSERT INTO comments (description, user_id, pic_id) value ('" + map[:description] + "', " + map[:user_id].to_s + ", " + map[:pic_id].to_s + ")"
-    #db.make_query(sql)
     get_connection.make_query(sql)
     close_connection
     
@@ -27,19 +19,8 @@ class Comment < Model
     comments = get_connection.make_query("SELECT * FROM comments where pic_id=" + picture_id.to_s, true)
     res = map_data(comments)
     close_connection
+    
     return res
-    
-    #db = MysqlConnect.new
-    #comments = db.make_query("SELECT * FROM comments where pic_id=" + picture_id.to_s, true)
-    
-    #res = []
-    #comments.each_hash {|row|
-    #  map={:description=>row['description'], :user_id=>row['user_id'], :pic_id=>row['pic_id']}
-    #  res << map
-    #}
-  
-    #db.close
-    #return res
   end  
   
   def validate_add(map)  
