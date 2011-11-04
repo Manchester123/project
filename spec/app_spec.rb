23df2d96f0c5963f55ce9440f1432de30e536f99
= <=>
2011-11-04 01:28:57 +0000

    changes on tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -18,7 +18,7 @@ describe "App Controller" do
 	last_response.body.should include("input")
     end
     
-    it "should call try_login method" do
+    it "should call try_login method, when requesting /login" do
 	post '/login', params = {:username => 'darik', :password => 'something'}
 	user = User.new
 	user.should respond_to(:try_login)
@@ -40,12 +40,12 @@ describe "App Controller" do
 	last_response.should be_redirect
     end
     
-    it "should redirect if parameters for registratino invalid (username)" do
+    it "should redirect if parameters for registration invalid (username)" do
 	post '/register', params = {:username => 'darik', :pwd => "123", :cpwd => "123"}
 	last_response.should be_redirect
     end
     
-    it "should call register method and redirect to / if parameters are valid" do
+    it "should call register method" do
 	post '/register', params = {:username => 'darik', :pwd => "123543", :cpwd => "123543"}
 	user = User.new
 	user.should respond_to(:register)
@@ -53,7 +53,7 @@ describe "App Controller" do
 
   it "should redirect to / after successfull registration of the user" do
     post '/register', params = {:username => 'someone', :pwd => "someone123", :cpwd => "someone123"}
-    last_resopnse.should be_redirect
+    last_response.should be_redirect
   end
     
     it "should redirect if session has already been set and user tries to get to register page" do
@@ -64,7 +64,7 @@ describe "App Controller" do
     
     it "should redirect if session has already been set and user tries to get login page" do
 	post '/login', params = {:username => "robrob", :password => "robrob"}#change password or username to fail the test
-	get '/'
+	get '/' #our login page requested as /
 	last_response.should be_redirect
     end
     
@@ -83,27 +83,35 @@ describe "App Controller" do
 	last_response.should be_redirect
     end
     
-    it "should invoke add_comment method and refresh the page" do
+    it "should invoke add_comment method" do
 	post '/comment', params = {:user_id => '1', :pic_id => '1', :description => "something"}
 	comment = Comment.new
 	comment.should respond_to(:add_comment)
 	last_response.should be_redirect
     end
+    it "should refresh the current page if comment has been successfully added" do
+      post '/comment', params = {:user_id => '1', :pic_id => '1', :description => "something"}
+      last_response.should be_redirect
+    end
     
-    it "should call get_comment and find_by_id methods on comments and photos object correspondently" do
+    it "should call get_comment method on comment object, when accessing view page" do
 	get '/view', params = {:id => 11}
 	comment = Comment.new
 	comment.should respond_to(:get_comment)
-	
-	photo = Photo.new
-	photo.should respond_to(:find_by_id)
     end
-    
+  
+  it "should call find_by_id method on photo object, when accessing view page" do
+    photo = Photo.new
+  	photo.should respond_to(:find_by_id)
+  end
     
   it "should response to search page" do
-    post '/search/1/winnie', params = {:category => 1, :words => 'winnie'}
+    post '/search', params = {:category => 1, :words => 'winnie'}
     last_response.should be_ok
-    
+  end
+  
+  it "should invoke search method, when accessing search with parameters" do
+    post '/search/1/winnie', params = {:category => 1, :words => 'winnie'}
     search_util = SearchPhoto.new
     search_util.should respond_to(:search)
   end
